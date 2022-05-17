bec9aeb3f8b18c3957a088832c96e2b434cb5a62
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-05-17 22:45:45 +0200

    shop page product page

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const menuBar = document.getElementById("menu-bar");
+const nav = document.getElementById("navbar");
+
+if (menuBar) {
+  menuBar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+console.log(nav);
