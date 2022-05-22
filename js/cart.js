22d478a02403d83bd4e72ee002fcfb1c351e4af3
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-05-22 13:59:03 +0200

    update

---
+++ js/cart.js
@@ -0,0 +1,5 @@
+$(".like-btn").on("click", function () {
+  $(this).toggleClass("is-active");
+});
+
+console.log("hello");
--- js/script.js
+++ js/script.js
@@ -1,12 +1,58 @@
-console.log(hello, Ben);
-
-const menuBar = document.getElementById("menu-bar");
+// Global variables
+const menuToggle = document.getElementById("menu-toggle-icon");
+const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
 
-if (menuBar) {
-  menuBar.addEventListener("click", () => {
+console.log(menuToggle);
+
+if (menuToggle) {
+  menuToggle.addEventListener("click", () => {
     nav.classList.add("active");
   });
 }
 
-console.log(nav);
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
+//
+//console.log(navMobile);
+//
+////Toggle navigation
+//const toggleMenu = () => {
+//  nav.classList.toggle("active");
+//  headerElement.classList.toggle("active");
+//};
+//
+//menuToggleIcon.addEventListener("click", toggleMenu);
+//
+//// add / remove header border bottom on scroll
+//const headerScrollEvent = () => {
+//  if (this.scrollY >= 30) {
+//    headerElement.classList.add("active-scroll");
+//  } else {
+//    headerElement.classList.remove("active-scroll");
+//  }
+//};
+//
+//window.addEventListener("scroll", headerScrollEvent);
+//
+//// Add selected link styles to the link clicked and close navigation when clicking the links
+
+//var mainImage = document.getElementById("mainImage");
+//var smallImage = document.getElementsByClassName("small-image");
+//smallImage[0].onclick = function () {
+//  mainImage.src = smallImage[0].src;
+//};
+//smallImage[1].onclick = function () {
+//  mainImage.src = smallImage[1].src;
+//};
+//smallImage[2].onclick = function () {
+//  mainImage.src = smallImage[2].src;
+//};
+//smallImage[3].onclick = function () {
+//  mainImage.src = smallImage[3].src;
+//};
+//console.log(mainImage);
