cc282c8e490f88fa24aa385b01626a71a13f9e87
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-05-25 09:04:52 +0200

    Javascript file

--- js/index.js
+++ js/index.js
@@ -19,39 +19,73 @@ window.onload = function () {
     cartBok.classList.remove("active");
   });
 
-  // Local Storage  check the
-
-  if (typeof Storage !== "undefined") {
-    //const array = ["a", "b", "c", "d"];
-    //const object = [
-    //  {
-    //    name: "Ousman Didi",
-    //    channelName: "fullyworld web",
-    //    address: {
-    //      hosueNo: "0259",
-    //    },
-    //  },
-    //  {
-    //    name: "Amat  foo",
-    //    channelName: "fullyworld web",
-    //    address: {
-    //      hosueNo: "0259",
-    //    },
-    //  },
-    //];
-    //localStorage.setItem("localStorageinfo", JSON.stringify(object));
-    ////console.log("our storage is working");
-    //localStorage.setItem("localStorageinfo", array);
-    //localStorage.setItem("localStorageinfo", 37);
-    //// get data from local storage
-
-    //const data = localStorage.getItem("localStorageinfo");
-    //console.log(JSON.parse(data)[0].channelName);
-
-    //console.log(data[2]);
-    //console.log(data);
-    localStorage.removeItem(localStorageinfo);
-  } else {
-    console.log("local storage is not working on your browser!");
+  //  Adding data to Local Storage
+  const addToCartBtn = document.getElementsByClassName("addToCart");
+  //let number = JSON.getItem()
+  let items = [];
+  console.log(addToCartBtn);
+  for (let i = 0; i < addToCartBtn.length; i++) {
+    addToCartBtn[i].addEventListener("click", function (e) {
+      console.log(e.target.parentElement.children[1].children[0].textContent);
+      if (typeof Storage !== "undefined") {
+        let item = {
+          id: i + 1,
+          name: e.target.parentElement.children[0].textContent,
+          price: e.target.parentElement.children[1].children[0].textContent,
+          number: 1,
+        };
+        if (JSON.parse(localStorage.getItem("items")) === null) {
+          items.push(item);
+          localStorage.setItem("items", JSON.stringify(items));
+        } else {
+          //alert("item is present");
+          const localItems = JSON.parse(localStorage.getItem("items"));
+          localItems.map((data) => {
+            if (item.id == data.id) {
+              item.number = data.number + 1;
+            } else {
+              items.push(data);
+            }
+          });
+          items.push(item);
+          localStorage.setItem("items", JSON.stringify(items));
+          window.location.reload();
+        }
+      } else {
+        alert("local storage is not working on your browser!");
+      }
+    });
   }
+  // adding the data to the shopping cart
+
+  const iconShoppingP = document.querySelector(".iconShopping p");
+  let number = 0;
+  JSON.parse(localStorage.getItem("items")).map((data) => {
+    number = number + data.number;
+  });
+  iconShoppingP.innerHTML = number;
+
+  // here  adding  cartBok data in the table
+  const cartBokTable = cartBok.querySelector("table");
+  //console.log(cartBokTable);
+  let tableData = "";
+  tableData += `<tr> <th>S no</th> <th>Item Name</th><th>Item No</th><th>Item Price</th><th></th></tr>`;
+
+  if (JSON.parse(localStorage.getItem("items")) === null) {
+    tableData += `<tr> <td colspan="5">No items Found</td></tr>`;
+  } else {
+    JSON.parse(localStorage.getItem("items")).map((data) => {
+      tableData +=
+        `<tr> <th>` +
+        data.id +
+        `</th><th>` +
+        data.name +
+        `</th><th>` +
+        data.no +
+        `</th><th>` +
+        data.price +
+        `</th><th><a href="#">Delete</a></th></tr>`;
+    });
+  }
+  cartBokTable.innerHTML = tableData;
 };
