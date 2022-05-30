a7630ae6d2386603ac7be1e5d2e32a91251766bf
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-05-30 13:23:45 +0200

    Login drop down menu

--- js/ui.js
+++ js/ui.js
@@ -1,31 +1,15 @@
-var changeImage = document.getElementById("product-details");
-var smallImage = document.getElementsByClassName(".small-image");
+const arrowUp = document.querySelector(".arrow-up");
+const form = document.querySelector(".login-from");
+const login = document.querySelector("#login");
 
-function changeImage() {
-  let displayImage = document.getElementById("product-details");
-  if (displayImage.src.match("product-details"));
-  {
-    displayImage.src = smallImage;
-  }
-  {
-    displayImage.src = "smallImage";
-  }
-}
+$(document).ready(function () {
+  const arrowUp = $(".arrow-up");
+  const form = $(".login-from");
+  const static = false;
+  $("#login").click(function (even) {
+    even.preventDefault();
+    alert(0);
+  });
+});
 
-changeImage();
-
-//console.log(productDetails);
-//smallImage[0].onclick = function () {
-//  productDetails.src = smallImage[0].src;
-//};
-//
-//smallImage[1].onclick = function () {
-//  productDetails.src = smallImage[1].src;
-//};
-//smallImage[2].onclick = function () {
-//  productDetails.src = smallImage[2].src;
-//};
-//smallImage[3].onclick = function () {
-//  productDetails.src = smallImage[3].src;
-//};
-//
+arrowUp = function () {};
