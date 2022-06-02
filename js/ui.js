d4d3ffbb570bb1d67183d3b75bdfc13f29ea9fa6
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-06-02 12:22:10 +0200

    checkout page link to contact pages

--- js/ui.js
+++ js/ui.js
@@ -20,58 +20,59 @@ document.addEventListener("click", (e) => {
 
 //const form = document.querySelector("#contactForm");
 
-const userName = document.querySelector("#username");
-const userNameError = document.querySelector("#userNameError");
-
-const passWord = document.querySelector("#passWord");
-const passwordError = document.querySelector("#passwordError");
-
-const email = document.querySelector("#email");
-const emailError = document.querySelector("#emailError");
-
-console.log(userName);
-function validateForm(event) {
-  event.preventDefault();
-
-  //console.log("hello");
-
-  if (checkLength(userName.value, 5) === true) {
-    userNameError.style.display = "none";
-  } else {
-    userNameError.style.display = "block";
-  }
-
-  if (checkLength(passWord.value, 6) === true) {
-    passwordError.style.display = "none";
-  } else {
-    passwordError.style.display = "block";
-  }
-
-  if (validateEmail(email.value, 24) === true) {
-    emailError.style.display = "none";
-  } else {
-    emailError.style.display = "block";
-  }
-
-  //if (checkLength(message.value, 0) === true) {
-  //  messageError.style.display = "none";
-  //} else {
-  //  messageError.style.display = "block";
-  //}
-}
-
-form.addEventListener("login", validateForm);
-
-function checkLength(value, len) {
-  if (value.length > len) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function validateEmail(email) {
-  const regEx = /\S+@\S+\.\S+/;
-  const patternMatches = regEx.test(email);
-  return patternMatches;
-}
+//const userName = document.querySelector("#username");
+//const userNameError = document.querySelector("#userNameError");
+//
+//const passWord = document.querySelector("#passWord");
+//const passwordError = document.querySelector("#passwordError");
+//
+//const email = document.querySelector("#email");
+//const emailError = document.querySelector("#emailError");
+//
+//console.log(userName);
+//function validateForm(event) {
+//  event.preventDefault();
+//
+//  //console.log("hello");
+//
+//  if (checkLength(userName.value) === true) {
+//    userNameError.style.display = "none";
+//  } else {
+//    userNameError.style.display = "block";
+//  }
+//
+//  if (checkLength(passWord.value, 6) === true) {
+//    passwordError.style.display = "none";
+//  } else {
+//    passwordError.style.display = "block";
+//  }
+//
+//  if (validateEmail(email.value, 24) === true) {
+//    emailError.style.display = "none";
+//  } else {
+//    emailError.style.display = "block";
+//  }
+//
+//  //if (checkLength(message.value, 0) === true) {
+//  //  messageError.style.display = "none";
+//  //} else {
+//  //  messageError.style.display = "block";
+//  //}
+//}
+//
+//form.addEventListener("login", validateForm);
+//
+//function checkLength(value, len) {
+//  if (value.length > len) {
+//    return true;
+//  } else {
+//    return false;
+//  }
+//}
+//
+//function validateEmail(email) {
+//  const regEx = /\S+@\S+\.\S+/;
+//  const patternMatches = regEx.test(email);
+//  return patternMatches;
+//}
+//
