dea0c368c91cb804ace13ad299d192078a7e2b05
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-06-03 12:58:07 +0200

    Media done!

--- js/form.js
+++ js/form.js
@@ -1,3 +1,20 @@
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+  console.log("menuContent");
+});
+
 const form = document.querySelector("#contactForm");
 const firstName = document.querySelector("#firstName");
 const firstNameError = document.querySelector("#firstNameError");
